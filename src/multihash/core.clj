6dc72daa4587170cf34f7f1e1466bf9323a0eac5
Greg Look <greg@greg-look.net>
2015-11-03 13:50:45 -0800

    On second thought, remove select.

--- src/multihash/core.clj
+++ src/multihash/core.clj
@@ -323,21 +323,3 @@
       (throw (RuntimeException.
                (str "No supported hashing function for algorithm "
                     (:algorithm mhash) " to validate " mhash))))))
-
-
-(defn select
-  "Selects multihash identifiers from a sequence based on some criteria.
-
-  Available options:
-
-  - `:algorithm`  only return hashes using this algorithm
-  - `:encoder`    function to encode multihashes with (default: `hex`)
-  - `:after`      start enumerating ids lexically following this string
-  - `:prefix`     only return ids starting with the given string"
-  [opts ids]
-  (let [{:keys [algorithm prefix encoder], :or {encoder hex}} opts
-        after (:after opts prefix)]
-    (cond->> ids
-      algorithm  (filter #(= algorithm (:algorithm %)))
-      after      (drop-while #(pos? (compare after (encoder %))))
-      prefix     (take-while #(.startsWith ^String (encoder %) prefix)))))
